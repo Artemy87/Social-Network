c912a687ef9c7bd01efb36fec7a47a54ecf86cc0
Artemy <artemij87work@gmail.com>
2022-03-17 16:02:17 +0300

    editing styles

--- src/Profile/Dialogs/Dialogs.tsx
+++ src/Profile/Dialogs/Dialogs.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
-import {log} from "util";
+
 
 export const Dialogs = () => {
+
     return (
-        <div className={styles.dialogs}>
-            Dialogs
+        <div className={styles.container}>
+            <div className={styles.title}>Dialogs</div>
+            <div className={styles.dialogsContainer}>
+                <div className={styles.dialogs}>
+                    <div className={styles.dialog}>Dima</div>
+                    <div className={styles.dialog}>Valera</div>
+                    <div className={styles.dialog}>Inna</div>
+                    <div className={styles.dialog}>Victor</div>
+                </div>
+                <div className={styles.messages}>
+                    <div>My name's Denis</div>
+                    <div>How are you?</div>
+                    <div>Yo!</div>
+                    <div>Hi</div>
+                    <div>My number +0034024</div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Profile/MyPosts/MyPosts.tsx
+++ src/Profile/MyPosts/MyPosts.tsx
@@ -5,15 +5,17 @@ import styles from './MyPosts.module.css';
 export const MyPosts = () => {
     return (
         <div className={styles.myPosts}>
-            <Post title='Post1' name='Alex' message='Hi! How are you?' like={8}/>
-            <Post title='Post2' name='Anna' message='Hello! My number +231314' like={33}/>
-            <Post title='Post3' name='Victor' message='My names Victor' like={15}/>
+            <div className={styles.post}>
+                <Post title='Post1' name='Alex' message='Hi! How are you?' like={8}/>
+                <Post title='Post2' name='Anna' message='Hello! My number +231314' like={33}/>
+                <Post title='Post3' name='Victor' message='My names Victor' like={15}/>
+            </div>
             <div className={styles.postForm}>
+            <div className={styles.sendMessageWindow}>
                 <div>My post</div>
-                <div className={styles.sendMessageWindow}>
-                    <textarea name="" id=""></textarea>
-                    <button>add post</button>
-                </div>
+                <textarea name="" id=""></textarea>
+                <button>add post</button>
+            </div>
             </div>
         </div>
     )
